7fc9c549bc16eebccbe4a74093eff04e8dd2c302
Aayush <aayushvaghela12@gmail.com>
2024-09-17 17:52:26 +0530

    update

--- main/scriptwebsite.js
+++ main/scriptwebsite.js
@@ -1,6 +1,6 @@
 function a(){
-    window.location="../loginpage/index.html"
+    window.location="../index.html"
 }
 function signup(){
-    window.location="../loginpage/index.html"
+    window.location="../index.html"
 }
