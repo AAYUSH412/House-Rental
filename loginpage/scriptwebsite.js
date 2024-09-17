e9f4397ed79678e518068b09862e3d510ce7344c
AAYUSH412 <vaghelaaayush412@gmail.com>
2024-09-17 16:52:38 +0530

    Refactor Firebase imports and update Firebase configuration

--- loginpage/scriptwebsite.js
+++
@@ -1,6 +0,0 @@
-function a(){
-    window.location="index.html"
-}
-function signup(){
-    window.location="index.html"
-}
---
+++ main/scriptwebsite.js
@@ -0,0 +1,6 @@
+function a(){
+    window.location="../../loginpage/index.html"
+}
+function signup(){
+    window.location="../loginpage/index.html"
+}
